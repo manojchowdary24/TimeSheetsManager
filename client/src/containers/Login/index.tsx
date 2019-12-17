26253b33e25b3ade6ebd446059425f6995198400
Jeffrey Asmus <jeffreyasmus@Jeffreys-MacBook-Pro.local>
2019-12-17 13:14:21 -0800

    Add cache persistence

--- client/src/containers/Login/index.tsx
+++ client/src/containers/Login/index.tsx
@@ -6,6 +6,18 @@ import LoginForm from "../../components/Login";
 const Login = () => {
   const [login] = useMutation(LoginMutation, {
     ignoreResults: true,
+    update: (
+      cache,
+      {
+        data: {
+          login: { accessToken = "" },
+        },
+      },
+    ) => {
+      cache.writeData({
+        data: { isAuthenicated: !!accessToken },
+      });
+    },
   });
 
   const onSubmit = (data: any) => login({ variables: { input: { ...data } } });
