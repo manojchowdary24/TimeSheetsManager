cb7dbf36c6bfce7dfd6083f79eb1c1df82f80370
manoj kumar <manojchowdary2494@gmail.com>
2020-05-07 20:41:11 -0700

    new changes

--- client/src/containers/ForgotPassword/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ForgotPassword: React.FC = () => {
-  return <div>Forgot password page</div>;
-};
-
-export default ForgotPassword;
--- client/src/containers/RequestAccess/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RequestAccess: React.FC = () => {
-  return <div>Request Access page</div>;
-};
-
-export default RequestAccess;
