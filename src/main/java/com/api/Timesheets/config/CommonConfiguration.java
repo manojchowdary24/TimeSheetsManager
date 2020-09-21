580f95ff1f4b70fec36065faba0ab9313c0a31fd
manoj kumar <manojchowdary2494@gmail.com>
2020-09-21 16:19:10 -0700

    Adding legacyCookieProcessor

--- src/main/java/com/api/Timesheets/config/CommonConfiguration.java
+++ src/main/java/com/api/Timesheets/config/CommonConfiguration.java
@@ -1,5 +1,8 @@
 package com.api.Timesheets.config;
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -19,9 +22,9 @@ public class CommonConfiguration {
    *
    * @return
    */
-//  @Bean
-//  public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
-//    return (factory) -> factory.addContextCustomizers(
-//        (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
-//  }
+  @Bean
+  public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+    return (factory) -> factory.addContextCustomizers(
+        (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
+  }
 }
