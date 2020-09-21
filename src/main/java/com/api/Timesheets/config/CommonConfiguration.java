5984d88e7978273483ecd90ce1ff51be9e75ad6e
manoj kumar <manojchowdary2494@gmail.com>
2020-09-20 23:14:28 -0700

    Adding legacyCookieProcessor

--- src/main/java/com/api/Timesheets/config/CommonConfiguration.java
+++ src/main/java/com/api/Timesheets/config/CommonConfiguration.java
@@ -1,5 +1,8 @@
 package com.api.Timesheets.config;
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +13,18 @@ public class CommonConfiguration {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
+  }
+
+  /**
+   * Solve the problem:
+   * There was an unexpected error (type=Internal Server Error, status=500).
+   * An invalid domain [.xxx.com] was specified for this cookie
+   *
+   * @return
+   */
+  @Bean
+  public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+    return (factory) -> factory.addContextCustomizers(
+        (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
   }
 }
